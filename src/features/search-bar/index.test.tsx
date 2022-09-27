36348206c6a58973bc6284a3cab672d40e2616b5
Hanna Papova <seal.annuska@gmail.com>
2022-09-27 20:33:55 -0300

    refactor: home page tests

--- src/features/search-bar/index.test.tsx
+++ src/features/search-bar/index.test.tsx
@@ -1,41 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import SearchBar from '.';
-import { getSearchValueFromStorage, setSearchValueToStorage } from 'shared/helpers/storage';
 
 describe('Search bar', () => {
   it('renders component', () => {
     render(
       <SearchBar updateInputHandler={() => {}} clearInputHandler={() => {}} searchValue={null} />
     );
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument();
     expect(screen.getByTestId('search-input')).toBeEmptyDOMElement();
-    expect(screen.getByText(/search/i)).toBeInTheDocument();
-    expect(screen.getByPlaceholderText(/rick/i)).toBeInTheDocument();
-    expect(screen.getByDisplayValue('')).toBeInTheDocument();
-  });
-
-  it('set search value from props on rerender', async () => {
-    const SEARCH_VALUE = 'Rick';
-    const onChange = jest.fn();
-    const { rerender } = render(
-      <SearchBar updateInputHandler={onChange} clearInputHandler={() => {}} searchValue={null} />
-    );
-    expect(screen.getByTestId('search-input')).toBeEmptyDOMElement();
-
-    const input = screen.getByRole('searchbox');
-    userEvent.type(input, SEARCH_VALUE);
-    expect(onChange).toHaveBeenCalledTimes(4);
-
-    rerender(
-      <SearchBar
-        updateInputHandler={onChange}
-        clearInputHandler={() => {}}
-        searchValue={SEARCH_VALUE}
-      />
-    );
-    expect(screen.getByTestId('search-input')).toHaveDisplayValue(SEARCH_VALUE);
   });
 
   it('input has a focus on render', async () => {
@@ -45,53 +17,5 @@ describe('Search bar', () => {
 
     const input = getByTestId('search-input');
     expect(input).toHaveFocus();
-  });
-});
-
-describe('Local Storage for search bar', () => {
-  beforeAll(() => {
-    localStorage.clear();
-  });
-
-  it('On mounting input serch value should be get from the storage', () => {
-    const storedValue = getSearchValueFromStorage();
-
-    render(
-      <SearchBar
-        updateInputHandler={() => {}}
-        clearInputHandler={() => {}}
-        searchValue={storedValue}
-      />
-    );
-
-    expect(screen.getByTestId('search-input')).toBeEmptyDOMElement();
-  });
-
-  it('Input value should be saved to LocalStorage during component`s unmount', () => {
-    const TEST_VAL = 'test';
-    const storedValue = getSearchValueFromStorage();
-
-    const { unmount } = render(
-      <SearchBar
-        updateInputHandler={() => {}}
-        clearInputHandler={() => {}}
-        searchValue={storedValue}
-      />
-    );
-    expect(screen.getByTestId('search-input')).toBeEmptyDOMElement();
-    setSearchValueToStorage(TEST_VAL);
-
-    unmount();
-
-    const newStoredValue = getSearchValueFromStorage();
-    render(
-      <SearchBar
-        updateInputHandler={() => {}}
-        clearInputHandler={() => {}}
-        searchValue={newStoredValue}
-      />
-    );
-
-    expect(screen.getByTestId('search-input')).toHaveDisplayValue(TEST_VAL);
   });
 });
--- src/pages/home/index.test.tsx
+++ src/pages/home/index.test.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Home from '.';
+import { getSearchValueFromStorage, setSearchValueToStorage } from 'shared/helpers/storage';
 
 describe('Home', () => {
-  it('initial component renders with cards', async () => {
+  it('initially component renders with empty search input and default amount of cards', async () => {
     render(<Home />);
 
     const userList = await waitFor(() => screen.getAllByRole('listitem'));
     expect(userList).toHaveLength(8);
+    expect(screen.getByTestId('search-input')).toBeEmptyDOMElement();
   });
 
   it('filter cards with valid search input', async () => {
@@ -37,7 +39,7 @@ describe('Home', () => {
     await waitFor(() => screen.getByTestId('error-message'));
   });
 
-  it('clear search value and display default cards', async () => {
+  it('clear search input value and display all cards', async () => {
     render(<Home />);
     const input = screen.getByRole('searchbox');
     const clearInputButton = screen.getByRole('button');
@@ -47,5 +49,41 @@ describe('Home', () => {
     await waitFor(() => screen.getAllByRole('listitem'));
     expect(screen.getAllByRole('heading')).toHaveLength(8);
     expect(input).toHaveDisplayValue('');
+  });
+});
+
+describe('Local storage for the search component', () => {
+  beforeEach(() => {
+    localStorage.clear();
+  });
+
+  it('display empty input if there are no stored value in storage', () => {
+    render(<Home />);
+    expect(screen.getByTestId('search-input')).toBeEmptyDOMElement();
+    expect(getSearchValueFromStorage()).toBeNull();
+  });
+
+  it('stored value should be displayed in the input on component`s mount', () => {
+    const TEST_VAL = 'test';
+    setSearchValueToStorage(TEST_VAL);
+
+    render(<Home />);
+    expect(screen.getByTestId('search-input')).toHaveDisplayValue(TEST_VAL);
+  });
+
+  it('input value should be saved to LocalStorage during component`s unmount', () => {
+    const TEST_VAL = 'test1';
+
+    const { unmount } = render(<Home />);
+    const input = screen.getByRole('searchbox');
+
+    expect(screen.getByTestId('search-input')).toBeEmptyDOMElement();
+    userEvent.type(input, TEST_VAL);
+    setSearchValueToStorage(TEST_VAL);
+
+    unmount();
+
+    render(<Home />);
+    expect(screen.getByTestId('search-input')).toHaveDisplayValue(TEST_VAL);
   });
 });
