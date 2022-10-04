213b815ee09271455ef33e7e5280dec52991f309
Hanna Papova <seal.annuska@gmail.com>
2022-10-04 18:09:30 -0300

    feat: update form inputs

--- src/pages/form/index.test.tsx
+++ src/pages/form/index.test.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import Form from '.';
+import userEvent from '@testing-library/user-event';
+import { act } from 'react-dom/test-utils';
 
 describe('when Form component renders', () => {
   it('should have a form', () => {
@@ -14,9 +16,166 @@ describe('when Form component renders', () => {
   });
 });
 
-// describe('when typing in Form inputs', () => {
-//   it.todo('submit button should not be disabled', () => {
+describe('when typing in Form inputs', () => {
+  const TEST_DATA = {
+    firstName: 'Hanna',
+    lastName: 'Papova',
+    zipcode: '222222',
+    birthday: '1980-01-01',
+    country: 'Italy',
+    avatar: new File(['file'], 'file.png', { type: 'image/png' }),
+  };
+
+  it('submit button should not be disabled', () => {
+    render(<Form />);
+    const firstNameInput = screen.getByTestId('firstName');
+    userEvent.type(firstNameInput, TEST_DATA.firstName);
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+
+  it('should allow user to change first name', () => {
+    render(<Form />);
+    const firstNameInput = screen.getByTestId('firstName');
+    expect(firstNameInput).toBeEmptyDOMElement();
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    userEvent.type(firstNameInput, TEST_DATA.firstName);
+    expect(firstNameInput).toHaveDisplayValue(TEST_DATA.firstName);
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+
+  it('should allow user to change last name', () => {
+    render(<Form />);
+    const lastNameInput = screen.getByTestId('lastName');
+    expect(lastNameInput).toBeEmptyDOMElement();
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    userEvent.type(lastNameInput, TEST_DATA.lastName);
+    expect(lastNameInput).toHaveDisplayValue(TEST_DATA.lastName);
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+
+  it('should allow user to change a zipcode', () => {
+    render(<Form />);
+    const zipcodeInput = screen.getByTestId('zipcode');
+    expect(zipcodeInput).toBeEmptyDOMElement();
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    userEvent.type(zipcodeInput, TEST_DATA.zipcode);
+    expect(zipcodeInput).toHaveDisplayValue(TEST_DATA.zipcode);
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+
+  it('should allow user to change birthday date', () => {
+    render(<Form />);
+    const birthdayInput = screen.getByTestId('birthday');
+    expect(birthdayInput).toBeEmptyDOMElement();
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    userEvent.type(birthdayInput, TEST_DATA.birthday);
+    expect(birthdayInput).toHaveDisplayValue(TEST_DATA.birthday);
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+
+  it('should allow user to select a country', () => {
+    render(<Form />);
+    const countrySelect = screen.getByTestId('country');
+    expect(countrySelect).toHaveDisplayValue(/---/);
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    userEvent.selectOptions(countrySelect, TEST_DATA.country);
+    expect((screen.getByText(TEST_DATA.country) as HTMLOptionElement).selected).toBeTruthy();
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+
+  it('should allow user to upload an avatar', async () => {
+    render(<Form />);
+    const avatarInput: HTMLInputElement = screen.getByTestId('avatar');
+    expect(avatarInput).toBeEmptyDOMElement();
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    await act(async () => {
+      await waitFor(() => {
+        userEvent.upload(avatarInput, TEST_DATA.avatar);
+      });
+    });
+
+    if (!avatarInput.files) {
+      throw new Error('No files have been uploaded');
+    }
+
+    expect(avatarInput.files[0]).toStrictEqual(TEST_DATA.avatar);
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+
+  it('should allow user to select a gender', () => {
+    render(<Form />);
+    const genderInput: HTMLInputElement = screen.getByTestId('gender');
+    expect(genderInput.checked).toEqual(false);
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    userEvent.click(genderInput);
+    expect(genderInput.checked).toEqual(true);
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+  });
+
+  it('should allow user to agree on data processing', () => {
+    render(<Form />);
+    const agreementCheckbox: HTMLInputElement = screen.getByTestId('agreement');
+    expect(agreementCheckbox.checked).toEqual(false);
+    expect(screen.getByTestId('submit-button')).toBeDisabled();
+
+    userEvent.click(agreementCheckbox);
+    expect(agreementCheckbox.checked).toEqual(true);
+    expect(screen.getByTestId('submit-button')).not.toBeDisabled();
+  });
+});
+
+// describe('on submitting the Form', () => {
+//   const formData = {
+//     firstName: 'Hanna',
+//     lastName: 'Papova',
+//     zipcode: '222222',
+//     birthday: '',
+//     country: '',
+//   };
+
+//   it.todo('should display error if first name is invalid', async () => {
 //     render(<Form />);
-//     expect(screen.getByTestId('react-form')).toBeInTheDocument();
+
+//     const firstNameInput = screen.getByTestId('firstName');
+//     expect(firstNameInput).toBeInTheDocument();
+
+//     await userEvent.type(firstNameInput, 'h');
+//     await userEvent.click(screen.getByTestId('submit-button'));
+
+//     expect(firstNameInput).toHaveDisplayValue('h');
+//     expect(screen.getByText(/first name error message/i)).toBeInTheDocument();
+//   });
+
+//   it.todo('should display error if last name is invalid', async () => {
+//     render(<Form />);
+
+//     const lastNameInput = screen.getByTestId('lastName');
+//     expect(lastNameInput).toBeInTheDocument();
+
+//     await userEvent.type(lastNameInput, 'h');
+//     await userEvent.click(screen.getByTestId('submit-button'));
+
+//     expect(lastNameInput).toHaveDisplayValue('h');
+//     expect(screen.getByText(/last name error message/i)).toBeInTheDocument();
+//   });
+
+//   it.todo('should display error if zipcode is invalid', async () => {
+//     render(<Form />);
+
+//     const zipcodeInput = screen.getByTestId('zipcode');
+//     expect(zipcodeInput).toBeInTheDocument();
+
+//     await userEvent.type(zipcodeInput, 'h');
+//     await userEvent.click(screen.getByTestId('submit-button'));
+
+//     expect(zipcodeInput).toHaveDisplayValue('h');
+//     expect(screen.getByText(/zipcode error message/i)).toBeInTheDocument();
 //   });
 // });
--- src/pages/form/index.tsx
+++ src/pages/form/index.tsx
@@ -1,66 +1,159 @@
 import React from 'react';
 import COUNTRIES from 'shared/data/countries';
+import styles from './index.module.scss';
 
 type FormPropsType = {
   handleSubmit: () => void;
-  firstName: React.RefObject<HTMLInputElement>;
-  lastName: React.RefObject<HTMLInputElement>;
+  enableSubmitButton: () => void;
 };
 
 class Form extends React.Component {
+  firstNameInput: React.RefObject<HTMLInputElement>;
+  lastNameInput: React.RefObject<HTMLInputElement>;
+  zipcodeInput: React.RefObject<HTMLInputElement>;
+  birthdayInput: React.RefObject<HTMLInputElement>;
+  countrySelectOption: React.RefObject<HTMLSelectElement>;
+  avatarInput: React.RefObject<HTMLInputElement>;
+  genderInput: React.RefObject<HTMLInputElement>;
+  agreementInput: React.RefObject<HTMLInputElement>;
+
+  state = { disabled: true };
+
   constructor(props: FormPropsType) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
-    // this.firstName = React.createRef();
-    // this.lastName = React.createRef();
-    // this.zipcode = React.createRef();
-    // this.birthday = React.createRef();
-    // this.country = React.createRef();
+    this.enableSubmitButton = this.enableSubmitButton.bind(this);
+
+    this.firstNameInput = React.createRef();
+    this.lastNameInput = React.createRef();
+    this.zipcodeInput = React.createRef();
+    this.birthdayInput = React.createRef();
+    this.countrySelectOption = React.createRef();
+    this.avatarInput = React.createRef();
+    this.genderInput = React.createRef();
+    this.agreementInput = React.createRef();
   }
 
-  handleSubmit(event: React.FormEvent) {
+  handleSubmit(event: React.ChangeEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    const data = new FormData(event?.target);
+    console.log(Object.fromEntries(data.entries()));
+  }
+
+  enableSubmitButton() {
+    this.setState({
+      disabled: false,
+    });
   }
 
   render() {
     return (
       <div>
-        <h1>Form</h1>
-        <form onSubmit={this.handleSubmit} data-testid="react-form">
-          <label htmlFor="firstName" data-testid="firstName">
-            First Name
-          </label>
-          <input id="firstName" type="text" />
+        <form
+          className={styles.form}
+          onSubmit={this.handleSubmit}
+          data-testid="react-form"
+          autoComplete="off"
+        >
+          <label htmlFor="firstName">First Name</label>
+          <input
+            id="firstName"
+            name="firstName"
+            type="text"
+            data-testid="firstName"
+            ref={this.firstNameInput}
+            onChange={this.enableSubmitButton}
+            autoComplete="off"
+          />
 
-          <label htmlFor="lastName" data-testid="lastName">
-            Last Name
-          </label>
-          <input id="lastName" type="text" />
+          <label htmlFor="lastName">Last Name</label>
+          <input
+            id="lastName"
+            name="lastName"
+            data-testid="lastName"
+            type="text"
+            ref={this.lastNameInput}
+            autoComplete="off"
+            onChange={this.enableSubmitButton}
+          />
 
-          <label htmlFor="zipcode" data-testid="zipcode">
-            Zipcode
-          </label>
-          <input id="zipcode" type="text" />
+          <label htmlFor="zipcode">Zipcode</label>
+          <input
+            id="zipcode"
+            type="text"
+            name="zipcode"
+            data-testid="zipcode"
+            ref={this.zipcodeInput}
+            autoComplete="off"
+            onChange={this.enableSubmitButton}
+          />
 
-          <label htmlFor="birthday" data-testid="birthday">
-            Birthday
-          </label>
-          <input id="birthday" type="date" />
+          <label htmlFor="birthday">Birthday</label>
+          <input
+            id="birthday"
+            type="date"
+            name="birthday"
+            data-testid="birthday"
+            ref={this.birthdayInput}
+            autoComplete="off"
+            onChange={this.enableSubmitButton}
+          />
 
-          <label htmlFor="country" data-testid="country">
-            Country
-          </label>
-          <select id="country" value="" onChange={(e) => console.log(e.target.value)}>
-            <option key="default" value="" disabled={true}>
-              Please select your country
-            </option>
+          <label htmlFor="country">Country</label>
+          <select
+            id="country"
+            data-testid="country"
+            ref={this.countrySelectOption}
+            onChange={this.enableSubmitButton}
+            name="country"
+          >
+            <option hidden>---</option>
             {COUNTRIES.map((country) => (
               <option key={country} value={country}>
                 {country}
               </option>
             ))}
           </select>
-          <button type="submit" data-testid="submit-button" disabled={true}>
+
+          <label htmlFor="avatar">
+            Avatar
+            <input
+              type="file"
+              id="avatar"
+              name="avatar"
+              ref={this.avatarInput}
+              data-testid="avatar"
+              onChange={this.enableSubmitButton}
+              accept="image/png, image/jpeg, image/webp"
+            />
+          </label>
+
+          <label htmlFor="gender">
+            Switch
+            <input
+              id="gender"
+              name="gender"
+              type="checkbox"
+              data-testid="gender"
+              ref={this.genderInput}
+            />
+            <span />
+          </label>
+
+          <label htmlFor="agreement">
+            <input
+              id="agreement"
+              name="agreement"
+              type="checkbox"
+              ref={this.agreementInput}
+              data-testid="agreement"
+              onChange={this.enableSubmitButton}
+            />
+            I give my consent to processing my personal data
+          </label>
+
+          <button type="submit" data-testid="submit-button" disabled={this.state.disabled}>
             Submit
           </button>
         </form>
