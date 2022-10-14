091cb3292a2052f5a96dfd9c6f5ded7ef202dcb0
Hanna Papova <seal.annuska@gmail.com>
2022-10-14 15:56:22 -0300

    feat: add responsive layout for home card list

--- src/features/card-list/card/index.tsx
+++ src/features/card-list/card/index.tsx
@@ -15,15 +15,17 @@ class Card extends React.Component<ICardProps> {
   render() {
     const card = this.props.card;
     const img = `https://farm${card.farm}.staticflickr.com/${card.server}/${card.id}_${card.secret}.jpg`;
+    const title = card.title.length < 35 ? card.title : card.title.slice(0, 35) + '...';
+    const name = card.ownername.length < 39 ? card.ownername : card.ownername.slice(0, 39) + '...';
 
     return (
       <li className={styles.card} onClick={(event) => this.props.showPopUp(this.props.card, event)}>
         <img src={img} alt={card.title} className={styles.img} data-testid="card-image" />
         <h2 className={styles.title} data-testid="card-title">
-          {card.title}
+          {title}
         </h2>
         <span className={styles.subtitle} data-testid="card-subtitle">
-          {card.ownername}
+          {name}
         </span>
       </li>
     );
