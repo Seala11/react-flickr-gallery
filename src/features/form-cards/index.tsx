225a9522ac1e3296ed0f1e0b98de2c7fb6f089c6
Hanna Papova <seal.annuska@gmail.com>
2022-10-18 14:16:03 -0300

    refactor: form cards feature into func component

--- src/features/form-cards/index.tsx
+++ src/features/form-cards/index.tsx
@@ -3,34 +3,24 @@ import React from 'react';
 import FormCard from './card';
 import styles from './index.module.scss';
 
-type FormCardListProps = {
+type Props = {
   cards: FormCardType[];
 };
 
-class FormCardList extends React.Component<FormCardListProps> {
-  constructor(props: FormCardListProps) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <section className={styles.section}>
-        <h2 className={styles.title}>Submitted forms</h2>
-        {!this.props.cards.length && (
-          <p className={styles.title}>You have not submitted any form yet</p>
-        )}
-        <ul className={styles.wrapper}>
-          {this.props.cards.map((card, index) => {
-            return (
-              <li key={index} className={styles.card}>
-                <FormCard card={card} />
-              </li>
-            );
-          })}
-        </ul>
-      </section>
-    );
-  }
-}
+const FormCardList = ({ cards }: Props) => (
+  <section className={styles.section}>
+    <h2 className={styles.title}>Submitted forms</h2>
+    {!cards.length && <p className={styles.title}>You have not submitted any form yet</p>}
+    <ul className={styles.wrapper}>
+      {cards.map((card, index) => {
+        return (
+          <li key={index} className={styles.card}>
+            <FormCard card={card} />
+          </li>
+        );
+      })}
+    </ul>
+  </section>
+);
 
 export default FormCardList;
